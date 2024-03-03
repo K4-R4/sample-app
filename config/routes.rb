10ba2d6522c22fc1f3973d7123731149c1f73639
tkuramot <mabdman512@gmail.com>
2024-03-04 01:56:39 +0900

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'application#hello'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'application#hello'
 end
