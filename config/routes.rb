b775ec9b3c68582fbfcc5b667f5aef6d2344d7f7
tkuramot <mabdman512@gmail.com>
2024-03-10 22:51:11 +0900

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
