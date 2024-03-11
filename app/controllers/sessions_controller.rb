95f6db8255be1396dac32242ce6c73b5ee5b713b
tkuramot <mabdman512@gmail.com>
2024-03-12 02:52:00 +0900

    chapter 10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
+      forward_url = session[:forwarding_url]
       reset_session
       # remember_meが0の場合は、以前のログイン情報を削除する
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       log_in @user
-      redirect_to @user
+      redirect_to forward_url || @user, status: :see_other
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new', status: :unprocessable_entity
