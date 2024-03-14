98fd68fbbf6d5a116933cee6b4f49af6c5626c54
tkuramot <mabdman512@gmail.com>
2024-03-14 21:06:18 +0900

    chapter 13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
 
-  def hello
-    render html: 'hello, world!'
+  private
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url, status: :see_other
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'sessions/new'
-  get 'users/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -13,10 +9,16 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
+  get 'users/new'
+  get '/microposts', to: 'microposts#home'
 
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
   # resources :password_resets, only: %i([new] [create] [edit] [update])
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
